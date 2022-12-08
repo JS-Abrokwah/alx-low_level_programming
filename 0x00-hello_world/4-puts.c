dbc168ac1047a2214a80ccb5cbe60acbf147cc49
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-08 22:41:03 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * 4-puts.c; author: JS-Abrokwah
- * This program prints a string to the standard output using puts
- * function
+ * main - The program starts executing from here
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char str[] = "Programming is like building a multilingual puzzle";
+    char str[] = "\"Programming is like building a multilingual puzzle\n";
 
-    puts(str);
-    return (0);
+    puts(str); /*print the value of variable str to stdout*/
+    return (0); /*exit program with 0*/
 }
