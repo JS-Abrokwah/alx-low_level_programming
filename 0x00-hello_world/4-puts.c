140da626d60cbc2518f37756ee961fc44d5dbab2
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-08 21:54:31 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * 4-puts.c; author: JS-Abrokwah
+ * This program prints a string to the standard output using puts
+ * function
+ */
+int main(void)
+{
+    char str[] = "Programming is like building a multilingual puzzle";
+
+    puts(str);
+    return (0);
+}
