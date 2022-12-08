eb064eaa9d94bcd8ad327df72776a10892e12a52
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-08 15:11:19 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The program starts execution from here
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(str); /*print format to stdout*/
+	return (0); /*exit with success code 0*/
+}
