20d04fa55b2466a955d17d277f946510b00ee5b2
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-09 18:08:03 -0800

    Print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 48; /*Declare an int variable*/
+
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
