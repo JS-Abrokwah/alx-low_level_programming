ba0775d1d3a0c30b0450b11ae178e25e3cea8693
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-09 17:58:43 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0; /*Declare an int variable*/
+
+	while (c < 10)
+	{
+		printf("%d", c);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
