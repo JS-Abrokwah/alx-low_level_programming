8512f644982b7d8f95714f95bce1cc3149fd5bc3
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-13 13:24:58 -0800

    10 x alpbhabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints all aphabets in lowercase
+ * print_alphabet_x10 - Prints all aphabets in lowercase
  *
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i = 0; /*Define int variable i*/
 
