03d8968dab5c5aff47281c749f12a6c7601ed4d0
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-13 17:25:05 -0800

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Sum up two integers
+ * @m: first int parameter
+ * @n: second int parameter
+ *
+ * Return: The sum of m and n
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}
