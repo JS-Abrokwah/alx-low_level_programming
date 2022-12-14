423e588e48a3fc8b2aa8ba6da8620f9216d7b564
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-13 18:13:04 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,9 @@
  */
 void print_to_98(int n)
 {
-	while (n >= 98 || n <= 98)
+	while (n > 98 || n < 98)
 	{
-		printf("%d,", n);
+		printf("%d, ", n);
 		if (n < 98)
 			n++;
 		else if (n > 98)
@@ -20,4 +20,5 @@ void print_to_98(int n)
 		else
 			break;
 	}
+	printf("%d", n);
 }
