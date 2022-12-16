e3a0d43361ddca90af554eea5b5593f06b699446
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-16 13:43:52 -0800

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints fizzBuzz
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz ");
+		else if (num % 3 == 0 && num % 5 != 0)
+			printf("Fizz ");
+		else if (num % 3 != 0 && num % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
