e35a8e4cd425c88f9050631b0aaa6baf60fe17b1
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-16 13:04:52 -0800

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - print a square shape with #
+ * @size: the length of the square to be printed
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < size)
+		{
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
