74e9037dfa21483845fac0c4ee3cd5f8c134cc63
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-19 12:22:08 -0800

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of a pointer parameter
+ * @n: a pointer parameter to a variable
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
