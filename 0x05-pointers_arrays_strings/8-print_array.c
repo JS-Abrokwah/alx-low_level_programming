62c33449cf329a8ad300723bfd9f8d516136a76f
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-19 15:05:16 -0800

    Print int array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n number of array elements
+ * @a: the array to be used
+ * @n: the number of elements to be printed
+ *
+ * Reaturn: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		if (i == n)
+		{
+			printf("%d\n", *(a + i));
+			i++;
+			continue;
+		}
+		printf("%d, ", *(a + i));
+		i++;
+	}
+}
