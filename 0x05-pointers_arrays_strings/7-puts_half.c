bf3c5f2a6996b20fc3c925751958566f1a2aafb1
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2022-12-19 14:37:25 -0800

    prints upper half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string parameter
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int n;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	while (n < len)
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
