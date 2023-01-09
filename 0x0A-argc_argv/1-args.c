ebca06048fc3b296e79d0acdac1ab38df289a89f
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-09 04:37:07 -0800

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0, success code
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
