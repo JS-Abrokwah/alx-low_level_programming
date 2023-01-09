40b8c08969ba8dfca4be643169cf679b8aabf758
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-09 12:20:49 -0800

    Add runtime arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints the sum of all runtime arguments
+ * @argc: argument count
+ * @argv: array of runtime atguments
+ *
+ * Return: 1 on program failure, 0 on program success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, digitcheck;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		digitcheck = isdigit(argv[i]);
+		if (digitcheck == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
