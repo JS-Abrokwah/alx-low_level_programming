732841f1e5f6b0a8f3d211f1103f9c1fa6ac7cfb
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-16 14:18:19 -0800

    Init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize the members of a dog structure
+ * @d: the dog structure to init
+ * @name: name input for dog's name
+ * @age: age input for dog's age
+ * @owner: owner input for dog's owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
