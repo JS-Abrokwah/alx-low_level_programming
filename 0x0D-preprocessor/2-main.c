8bc59313002bf5f9ad54a14ac633d09b468d9f50
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-16 10:51:51 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Always 0 as success
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
