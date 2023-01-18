62eaba1036e5451b20c1583618146e9d295e22d9
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-18 05:26:12 -0800

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a callback on each array element
+ * @array: input array
+ * @size: size of input array
+ * @action: callback function pointer
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[1]);
+	}
+}
