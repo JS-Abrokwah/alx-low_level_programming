b62f82e3cbc98f5186276b5b251009cae9f5dd05
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-19 08:21:16 -0800

    Print optional arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print all optional arguments
+ * @separator: separator string
+ * @n: number of optional arguments
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int index;
+	va_list ap;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+
+	for (index = 0; index < n; index++)
+	{
+		if (index == n - 1)
+		{
+			printf("%d", va_arg(ap, int));
+			printf("\n");
+		}
+		else
+		{
+			printf("%d", va_arg(ap, int));
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+	}
+}
