02dc28bf7029dc720754a05c934fbe83278610d7
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-01-26 22:12:50 +0000

    Tortoise and hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * tortoise_n_hare - Prints a string before the
+ *        main function is executed.
+ */
+void tortoise_n_hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
